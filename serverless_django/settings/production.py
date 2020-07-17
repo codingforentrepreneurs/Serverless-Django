f8dc89c362621285806e29270013ccdecae16694
CFE <hello@teamcfe.com>
2020-07-17 11:06:27 -0700

    16 - Make Changes, Build, & Deploy

--- serverless_django/settings/production.py
+++ serverless_django/settings/production.py
@@ -9,13 +9,17 @@ ALLOWED_HOSTS = ['serverless-django-un3qlwrkgq-uw.a.run.app']
 DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", '+sn5=%=4*+da^!xvr5q%k$cqeov_@352ihfvzzxoc6$9o!x91d')
+SECRET_KEY = os.environ.get("SECRET_KEY", '+sn5=%=4*+da^!dfsr5q%k$cqeov_@352ihfvzzxoc6$9o!x91d')
 
 
 print("Using production")
 # Database
+DB_USER_UN = os.environ.get("DB_USER_UN", "")
+HOME_PAGE_MSG = f"Hello World. This Is Production."
 
-HOME_PAGE_MSG = "Hello World. This Is Production"
+DB_USER_PW = os.environ.get("DB_USER_PW", 'abc')
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
